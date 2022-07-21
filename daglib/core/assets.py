1eef2196e30de176d690bbb30bd1f7884f3abc04
Michael Harris <mharris@luabase.com>
2022-07-21 15:16:09 -0400

    Add `__name__` attr to Asset (#35)

--- daglib/core/assets.py
+++ daglib/core/assets.py
@@ -11,6 +11,9 @@ class Asset:
     def __call__(self) -> Any:
         return self.fn()
 
+    def __name__(self) -> str:
+        return self.fn.__name__
+
 
 def asset(fn: WrappedFn) -> Asset:
     @wraps(fn)
